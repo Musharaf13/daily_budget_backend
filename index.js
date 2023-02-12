53dbf37cf505cf0c17efada4ac956661acf70be9
muhammadmusharaf35@gmail.com <muhammadmusharaf35@gmail.com>
2023-02-12 10:11:13 +0500

    Expense Analytics and Expense History APIs created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("./app");
 
 process.env.TZ = "Asia/Karachi";
 const port = process.env.port || 4000;
-app.listen(port, "192.168.0.29", () => {
-  console.log("server running at: http://localhost:" + port);
+const localhost = "192.168.0.21";
+app.listen(port, localhost, () => {
+  console.log("server running at: http://" + localhost + ":" + port);
 });
--- routes/expanse.js
+++ routes/expanse.js
@@ -12,15 +12,71 @@ app.get("/", (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
-  const { categoryId, amount, description, userId } = req.body;
-
+  const { categoryId, amount, userId, created_at } = req.body;
+  var description = req.body.description ?? "";
   console.log("req body");
 
   console.log(req.body);
 
   var query = `INSERT INTO public.expenses(
          category_id, amount, description, user_id, created_at)
-        VALUES (${categoryId}, ${amount}, '${description}', ${userId}, '${utils.calTime()}');`;
+        VALUES (${categoryId}, ${amount}, '${description}', ${userId}, '${
+    created_at ?? utils.calTime()
+  }');`;
+  console.log(query);
+  pool.query(query, (error, result) => {
+    if (!error) {
+      res.send({
+        status: true,
+        data: result.rows,
+      });
+    } else {
+      res.send({
+        status: false,
+        message: "Failed to add Expense" + error.message,
+      });
+    }
+  });
+});
+
+app.post("/fetchExpansesAnalysis", async (req, res) => {
+  const { userId } = req.body;
+
+  console.log("req body");
+
+  console.log(req.body);
+
+  var query = `Select  Sum(amount) as amount, 
+  (Select Sum(amount)
+  from expenses
+  ) as total_expense,
+  To_Char(created_at::date,'dd') as date from expenses
+  where user_id='${userId}'
+  group by date
+  `;
+  console.log(query);
+  pool.query(query, (error, result) => {
+    if (!error) {
+      res.send({
+        status: true,
+        data: result.rows,
+      });
+    } else {
+      res.send({
+        status: false,
+        message: "Failed to add Expense" + error.message,
+      });
+    }
+  });
+});
+
+app.post("/fetchAllExpenses", async (req, res) => {
+  const { startDate, endDate, userId } = req.body;
+  console.log("req body");
+  console.log(req.body);
+  const categoryIds = req.body.categoryIds.join(",");
+  var query = `Select SUM(amount) over() as  total_expense,category_id,amount,description from expenses
+where (created_at::date  between '${startDate}'::date and '${endDate}'::date) and category_id in (${categoryIds}) and  user_id = ${userId}`;
   console.log(query);
   pool.query(query, (error, result) => {
     if (!error) {
