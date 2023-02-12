eafc919ac6a0d1b0996d181c77f58c7164514ca7
muhammadmusharaf35@gmail.com <muhammadmusharaf35@gmail.com>
2023-02-12 10:20:13 +0500

    Aligning for deployment

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require("./app");
 process.env.TZ = "Asia/Karachi";
 const port = process.env.port || 4000;
 const localhost = "192.168.0.21";
-app.listen(port, localhost, () => {
+app.listen(port, () => {
   console.log("server running at: http://" + localhost + ":" + port);
 });
